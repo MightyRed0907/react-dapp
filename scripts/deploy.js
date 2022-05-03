0ccb84b46fc46a48170ec8208a1f089d87d6bb9c
Red Mighty <oohs.0907@gmail.com>
2022-05-03 06:09:15 +0300

    Implement NDT

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -14,11 +14,16 @@ async function main() {
   const Token = await hre.ethers.getContractFactory("Token");
   const token = await Token.deploy();
 
+  const NDToken = await hre.ethers.getContractFactory("NDToken");
+  const ndToken = await NDToken.deploy("Nader Dabit Token", "NDT");
+
   await greeter.deployed();
   await token.deployed();
+  await ndToken.deployed();
 
   console.log("Greeter deployed to:", greeter.address);
   console.log("Token deployed to:", token.address);
+  console.log("NDToken deployed to:", ndToken.address);
 }
 
 main()
--- src/App.js
+++ src/App.js
@@ -3,14 +3,19 @@ import { useState } from 'react';
 import { ethers } from 'ethers'
 import Greeter from './artifacts/contracts/Greeter.sol/Greeter.json'
 import Token from './artifacts/contracts/Token.sol/Token.json'
+import NDToken from './artifacts/contracts/NDToken.sol/NDToken.json'
 
 const greeterAddress = "0x5FbDB2315678afecb367f032d93F642f64180aa3"
 const tokenAddress = "0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512"
+const ndTokenAddress = "0x5FbDB2315678afecb367f032d93F642f64180aa3"
 
 function App() {
   const [greeting, setGreetingValue] = useState()
   const [userAccount, setUserAccount] = useState()
   const [amount, setAmount] = useState()
+
+  const [userNDTAccount, setUserNDTAccount] = useState();
+  const [ndtAmount, setNDTAmount] = useState()
 
   async function requestAccount() {
     await window.ethereum.request({ method: 'eth_requestAccounts' });
@@ -66,6 +71,29 @@ function App() {
     }
   }
 
+  async function getBalanceOfNDT() {
+    if (typeof window.ethereum !== 'undefined') {
+      const [account] = await window.ethereum.request({ method: 'eth_requestAccounts'});
+      const provider = new ethers.providers.Web3Provider(window.ethereum);
+      const contract = new ethers.Contract(ndTokenAddress, NDToken.abi, provider);
+      const balance = await contract.balanceOf(account);
+      console.log("NDT Balance: ", balance.toString());
+    }
+  }
+
+  async function sendNDT() {
+    if (typeof window.ethereum !== 'undefined') {
+      await requestAccount();
+      const provider = new ethers.providers.Web3Provider(window.ethereum);
+      const signer = provider.getSigner();
+      const contract = new ethers.Contract(ndTokenAddress, NDToken.abi, signer);
+      const transaction = await contract.transfer(userNDTAccount, ndtAmount);
+      await transaction.wait();
+
+      console.log(`${ndtAmount} NDT successfully sent to ${userNDTAccount}`);
+    }
+  }
+
   return (
     <div className="App">
       <header className="App-header">
@@ -78,6 +106,13 @@ function App() {
         <button onClick={sendCoins}>Send Coins</button>
         <input onChange={e => setUserAccount(e.target.value)} placeholder="Account ID" />
         <input onChange={e => setAmount(e.target.value)} placeholder="Amount" />
+
+        <br/>
+        <button onClick={getBalanceOfNDT}>Get NDToken Balance</button>
+        <button onClick={sendNDT}>Send NDT</button>
+        <input onChange={e => setUserNDTAccount(e.target.value)} placeholder="Account ID" />
+        <input onChange={e => setNDTAmount(e.target.value)} placeholder="NDT Amount" />
+
       </header>
     </div>
   );
